1e0c333e83d5b05cbe8af37024001a6cb57ba194
Rafael Lopez <lopez.rafael08@gmail.com>
2021-10-29 19:08:27 -0400

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# SSO with Spring Boot + Keycloak
+
+A demo project to demonstrate how to use Spring Boot, Keycloak, Thymeleaf and Spring Security to implement Single Sign-on (SSO)
---
+++ src/test/java/com/lopez/rafael/studentapp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.studentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
