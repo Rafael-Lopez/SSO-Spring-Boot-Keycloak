ead192abcc203d4499d1fec0c7c3d50647c5e68e
Rafael Lopez <lopez.rafael08@gmail.com>
2021-11-01 12:41:02 -0400

    Authorization with Spring Security

--- src/main/java/com/lopez/rafael/studentapp/StudentAppApplication.java
+++ src/main/java/com/lopez/rafael/studentapp/StudentAppApplication.java
@@ -2,8 +2,10 @@ package com.lopez.rafael.studentapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StudentAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lopez/rafael/studentapp/controller/StudentController.java
+++ src/main/java/com/lopez/rafael/studentapp/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.lopez.rafael.studentapp.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,5 +16,14 @@ public class StudentController {
     @GetMapping("/home")
     public ModelAndView home() {
         return new ModelAndView("home");
+    }
+
+    @GetMapping("/manage-students")
+    @PreAuthorize("hasAuthority('PROFESSOR')")
+    // For multiple roles you have options:
+    // @PreAuthorize("hasAuthority('PROFESSOR') or hasAuthority('STUDENT')")
+    // @PreAuthorize("hasAuthority('PROFESSOR') and hasAuthority('STUDENT')")
+    public ModelAndView manageStudents() {
+        return new ModelAndView("manage-students");
     }
 }
