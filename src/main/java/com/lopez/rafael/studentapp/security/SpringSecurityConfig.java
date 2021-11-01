febc109cc6181c41415a6bd377201b93b3d7aede
Rafael Lopez <lopez.rafael08@gmail.com>
2021-11-01 12:15:38 -0400

    Add PasswordEncoder for Spring Security

--- src/main/java/com/lopez/rafael/studentapp/security/SpringSecurityConfig.java
+++ src/main/java/com/lopez/rafael/studentapp/security/SpringSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.lopez.rafael.studentapp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +33,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl);
+        auth
+            .userDetailsService(userDetailsServiceImpl)
+            .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
