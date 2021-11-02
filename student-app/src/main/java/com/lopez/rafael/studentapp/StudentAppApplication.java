8da322e53ecac0b721543425746cea996883de61
Rafael Lopez <lopez.rafael08@gmail.com>
2021-11-02 12:20:24 -0400

    Remove submodule added accidentally

---
+++ student-app/src/main/java/com/lopez/rafael/studentapp/StudentAppApplication.java
@@ -0,0 +1,15 @@
+package com.lopez.rafael.studentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class StudentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppApplication.class, args);
+	}
+
+}
---
+++ student-app/src/main/java/com/lopez/rafael/studentapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lopez.rafael.studentapp.repository;
+
+import com.lopez.rafael.studentapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
