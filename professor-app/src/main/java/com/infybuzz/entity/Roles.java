a7653d3906bedc6986e6f9a0043259ccbecb21f4
Rafael Lopez <lopez.rafael08@gmail.com>
2021-11-02 12:07:57 -0400

    Replace embedded H2 db with MySQL + Update projects to run against MySQL + Move db scripts to root folder

--- professor-app/src/main/java/com/infybuzz/entity/Roles.java
+++ professor-app/src/main/java/com/infybuzz/entity/Roles.java
@@ -8,7 +8,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-@Table(name = "roles")
+@Table(name = "role")
 @Entity
 public class Roles {
 
